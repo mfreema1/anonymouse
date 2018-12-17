589174c35803f3221f85c93bc9525a8a431b45bf
mfreema1 <mfreema1@stevens.edu>
2018-12-17 15:02:36 -0500

    Add credential validation to professor route

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -1,7 +1,8 @@
 const wss = require('./wss')
-wss.createServer();
+const credentials = require('./credentials')
 
 module.exports = (app) => {
+
     app.get('/professor', (req, res) => {
         //provide form page to create room
     })
@@ -15,8 +16,14 @@ module.exports = (app) => {
         const usn = req.body.username
         const pwd = req.body.password
 
-        //if the credentials are correct, spawn a room, give the id back to the professor
-        const roomCode = await wss.createRoom()
-        res.send(roomCode)
+        //authenticate
+        const isValid = await credentials.validate(usn, pwd)
+
+        if(isValid) {
+            //if the credentials are correct, spawn a room, give the id back to the professor
+            const roomCode = await wss.createRoom()
+            res.send(roomCode)
+        }
+        else res.send('Credentials invalid')
     })
 }
