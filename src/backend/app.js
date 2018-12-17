0ea8e52a2bc5e092f5b8834bf21aed5c09319308
mfreema1 <mfreema1@stevens.edu>
2018-12-17 15:01:28 -0500

    Switch to json for POSTs

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const port = 3000 //wss on 8080, http on 3000
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 require('./routes')(app)
 
 app.listen(port, () => { console.log(`Server started on port ${port}`) })
