e1e54b666ee3348c3c4455a0d8d113c90cb210da
mfreema1 <mfreema1@stevens.edu>
2018-12-19 23:27:42 -0500

    Move request docs, flesh out structure

--- src/backend/wss.js
+++ src/backend/wss.js
@@ -1,6 +1,7 @@
 const WebSocket = require('ws')
 const Promise = require('bluebird').Promise
 const redis = Promise.promisifyAll(require('redis'))
+const processMessage = require('./msg/messageProcessor')
 const port = 8080;
 
 function noop() {}
@@ -9,29 +10,53 @@ function heartbeat(ws) {
     ws.isAlive = true
 }
 
-//each websocket gets a redisClient.  We then wait for message
+//each websocket gets a redis client.  We then wait for message
 //events to that redis client.  Also, we use one redis client for
 //the web socket server itself
-const wss = new WebSocket.Server({ port })
-const redisClient = redis.createClient()
+//can set { clientTracking: false } to remove wss.clients
+const wss = new WebSocket.Server({ port }) //don't collect clients, we don't intend to broadcast like this
+const rootRedis = redis.createClient()
+ //keep track of the connected sockets
 
 //configure events on the server
 //wss is the websocket server
 //ws is the websocket client
 wss.on('connection', async (ws, req) => {
     ws.isAlive = true
+    //get unique identifier of connection
+
+    //when the connection is open, ask for a set of credentials (start login handshake)
+    //will need to include this in our message schemas as well
+
+    //this is because our application is not the only one that can act as a websocket client to this
+    //server.  Anyone knowing the username of a user could impersonate them by simply launching a websocket
+    //request to this server.  Must begin the interaction with a login.  What we can do is for the first message of a socket,
+    //check to see if it is a login attempt.  If it isn't drop them immediately.  If it 
+    
+    //firstly, make sure that this IP is not already connected
     const clientIP = req.connection.remoteAddress
-    const isMember = await redisClient.sismemberAsync('clients', clientIP)
+    const isMember = await rootRedis.sismemberAsync('clients', clientIP)
     if(isMember) {
         ws.send('Already connected, please stop')
         ws.close()
     }
     else {
-        await redisClient.saddAsync('clients', clientIP)
-        ws.send('Hey welcome!')
-        ws.on('close', () => { //if the client disconnects, remove from client pool
-            redisClient.sremAsync('clients', clientIP)
+        await rootRedis.saddAsync('clients', clientIP)
+        ws.authenticated = false //we wait until they are authed to check for anything but a login
+        ws.redis = redis.createClient()
+        //CONCLUSION: we need to give everyone a redis client, otherwise there is no real difference
+        //between simply storing a set on redis and using that to push updates via iteration
+
+        //if the client disconnects, remove from client pool
+        ws.on('close', () => { 
+            //looks like we may need to use function() to keep 'this' reference
+            //this.identifier remove from sockets
+            rootRedis.sremAsync('clients', clientIP)
         })
+
+        //listen for messages from a particular client
+        ws.on('message', (msg) => { processMessage(msg, ws, rootRedis) })
+
         ws.on('pong', () => { heartbeat(ws) })
     }      
 })
@@ -64,7 +89,7 @@ async function createRoom() {
 
     //recursively spawn more calls until we get a room that is free
     async function getRoom(randomString) {
-        const isTaken = await redisClient.sismemberAsync('rooms', randomString)
+        const isTaken = await rootRedis.sismemberAsync('rooms', randomString)
         if(isTaken) return getRoom(getRandomString())
         return randomString
     }
@@ -72,7 +97,7 @@ async function createRoom() {
     const randomString = await getRoom(getRandomString())
 
     //we have a free room, now mark it as taken
-    await redisClient.saddAsync('rooms', randomString)
+    await rootRedis.saddAsync('rooms', randomString)
     return randomString
 }
 
@@ -86,26 +111,3 @@ module.exports = {
 //3. Allow professors to accept/reject questions as they come in
 //4. Inform student of accept/reject
 //5. If accepted, publish to room channel
-
-//Proposed message formats
-
-//Student to professor
-/**
-    {
-        roomID: "",
-        username: "",
-        message: ""
-    }
- */
-
-//Once received, cache the message on the server until we get an
-//approval or denial from the professor, max of 30 minutes
-
-//Professor response to student
-/**
-    {
-        roomID: "",
-        messageID: "",
-        approved: bool
-    }
- */
