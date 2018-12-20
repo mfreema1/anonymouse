d0c5494c25bfc8dfb5f8f7b09f414786dc5c30b1
mfreema1 <mfreema1@stevens.edu>
2018-12-19 23:28:24 -0500

    Update client for most recent testing

--- test/client.js
+++ test/client.js
@@ -3,6 +3,13 @@ const WebSocket = require('ws')
 const ws = new WebSocket('ws://localhost:8080')
 ws.on('open', () => {
     console.log('Connected to server')
+    ws.send(JSON.stringify({
+        type: "login",
+        payload: {
+            username: 'oh',
+            password: 'hello'
+        }
+    }))
 })
 
 ws.on('message', (data) => {
