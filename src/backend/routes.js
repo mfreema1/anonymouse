12ce07ac5577ca8408712e6770a7c826deb4fe47
mfreema1 <mfreema1@stevens.edu>
2018-12-21 01:02:48 -0500

    Clean out http server

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -1,32 +1,3 @@
 const wss = require('./wss')
-const credentials = require('./credentials')
 
-module.exports = (app) => {
-
-    app.get('/professor', (req, res) => {
-        //provide form page to create room
-
-        //just serve a dummy file for now to test client side API
-        res.sendFile(`${__dirname}/dummyclient.html`)
-    })
-
-    app.get('/student', (req, res) => {
-        //provide form page to join room
-    })
-
-    app.post('/professor', async (req, res) => {
-        //examine credentials in body
-        const usn = req.body.username
-        const pwd = req.body.password
-
-        //authenticate
-        const isValid = await credentials.validate(usn, pwd)
-
-        if(isValid) {
-            //if the credentials are correct, spawn a room, give the id back to the professor
-            const roomCode = await wss.createRoom()
-            res.send(roomCode)
-        }
-        else res.send('Credentials invalid')
-    })
-}
+module.exports = (app) => {}
