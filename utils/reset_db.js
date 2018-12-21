290d0b5afffa30caced7ffab0fa5e19a7ff8ea66
mfreema1 <mfreema1@stevens.edu>
2018-12-21 00:58:41 -0500

    Update SQL table structure

--- utils/reset_db.js
+++ utils/reset_db.js
@@ -3,15 +3,19 @@ const dbConfig = require('../secret').dbConfig
 
 const drop_users = "DROP TABLE IF EXISTS users"
 //using SHA256 means that it is 256 bits -- 64 hex characters
-const make_users = "CREATE TABLE users (username CHAR(64), password CHAR(64))"
-//"oh", "hi"
-const dummy_creds = 'INSERT INTO users VALUES ("e3e710fdf45f3f8a0cae5c3f520d767abdff0f417892bb49a220e4962ccb643d", "d5c41f3af7fec8eed2ff49eab74c54751bc596a20bac0b8b78a9945666feaa83")'
+const make_users = "CREATE TABLE users (username CHAR(64), password CHAR(64), isProfessor BOOLEAN)"
+//"profDummy", "profDummyPass"
+const dummy_prof_creds = 'INSERT INTO users VALUES ("ea412a4dbccd6c5dfaba59a0030c19d3b37b76cfff1e7d0947a7c75872403275", "9a1b999b010e599c50d4205414d2c128e9d6b741ab06c4b1c9f981dfaadf01cf", true)'
+
+//"studentDummy", "studentDummyPass"
+const dummy_student_creds = 'INSERT INTO users VALUES ("b0559f7d7f2b32b717524102f156c4a256af38c4f1b2fba7c5bf1da44a64f0e9", "18fcccc0211813747ee2d8da3d0f8d6462a00a6fee72f7cc793291a6906d1f72", false)'
 
 async function reset(){
     const conn = await mariadb.createConnection(dbConfig)
     await conn.query(drop_users)
     await conn.query(make_users)
-    await conn.query(dummy_creds)
+    await conn.query(dummy_prof_creds)
+    await conn.query(dummy_student_creds)
     conn.end()
 }
 
