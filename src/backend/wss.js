bc3487f2755c4d1f22bf1b92bf669da6d45cdd8d
mfreema1 <mfreema1@stevens.edu>
2018-12-21 00:58:24 -0500

    Lighten up wss file

--- src/backend/wss.js
+++ src/backend/wss.js
@@ -10,60 +10,22 @@ function heartbeat(ws) {
     ws.isAlive = true
 }
 
-//each websocket gets a redis client.  We then wait for message
-//events to that redis client.  Also, we use one redis client for
-//the web socket server itself
-//can set { clientTracking: false } to remove wss.clients
-const wss = new WebSocket.Server({ port }) //don't collect clients, we don't intend to broadcast like this
-const rootRedis = redis.createClient()
- //keep track of the connected sockets
+const wss = new WebSocket.Server({ port })
+const rootRedis = redis.createClient() //each ws gets a redisClient, keep one redis client for the server
+wss.sockets = {} //map logged in usernames to sockets
 
-//configure events on the server
-//wss is the websocket server
-//ws is the websocket client
 wss.on('connection', async (ws, req) => {
     ws.isAlive = true
-    //get unique identifier of connection
+    ws.authenticated = false //wait until authed to give them a redis client
 
-    //when the connection is open, ask for a set of credentials (start login handshake)
-    //will need to include this in our message schemas as well
+    //since any client can send a websocket request to us, must authenticate all opened connections
 
-    //this is because our application is not the only one that can act as a websocket client to this
-    //server.  Anyone knowing the username of a user could impersonate them by simply launching a websocket
-    //request to this server.  Must begin the interaction with a login.  What we can do is for the first message of a socket,
-    //check to see if it is a login attempt.  If it isn't drop them immediately.  If it 
-    
-    //firstly, make sure that this IP is not already connected
-    const clientIP = req.connection.remoteAddress
-    const isMember = await rootRedis.sismemberAsync('clients', clientIP)
-    if(isMember) {
-        ws.send('Already connected, please stop')
-        ws.close()
-    }
-    else {
-        await rootRedis.saddAsync('clients', clientIP)
-        ws.authenticated = false //we wait until they are authed to check for anything but a login
-        ws.redis = redis.createClient()
-        //CONCLUSION: we need to give everyone a redis client, otherwise there is no real difference
-        //between simply storing a set on redis and using that to push updates via iteration
+    ws.on('message', (msg) => { processMessage(msg, ws, rootRedis, wss.sockets) })
 
-        //if the client disconnects, remove from client pool
-        ws.on('close', () => { 
-            //looks like we may need to use function() to keep 'this' reference
-            //this.identifier remove from sockets
-            rootRedis.sremAsync('clients', clientIP)
-        })
-
-        //listen for messages from a particular client
-        ws.on('message', (msg) => { processMessage(msg, ws, rootRedis) })
-
-        ws.on('pong', () => { heartbeat(ws) })
-    }      
+    ws.on('pong', () => { heartbeat(ws) })
 })
 
-//every 5 seconds, ping all clients for broken connections,
-//if we didn't hear back from the last ping in the time limit,
-//drop them
+//every 5 seconds, ping all clients for broken connections
 setInterval(() => {
     wss.clients.forEach((ws) => {
         //set the flag to false, a response (pong) will set back to true
@@ -72,38 +34,6 @@ setInterval(() => {
         ws.ping(noop)
     })
 }, 5000)
-
-//credentials for the professor have been validated, really all we need to do is generate a random
-//room code and go check to see if that room is taken.  Keep doing this until we get one that isn't
-//taken, then mark it as taken
-async function createRoom() {
-
-    function getRandomLetter() {
-        const letters = 'abcdefghijklmnopqrstuvwxyz'
-        return letters[Math.floor((Math.random() * letters.length))]
-    }
-
-    function getRandomString() {
-        return Array(4).fill().map(getRandomLetter).join('')
-    }
-
-    //recursively spawn more calls until we get a room that is free
-    async function getRoom(randomString) {
-        const isTaken = await rootRedis.sismemberAsync('rooms', randomString)
-        if(isTaken) return getRoom(getRandomString())
-        return randomString
-    }
-
-    const randomString = await getRoom(getRandomString())
-
-    //we have a free room, now mark it as taken
-    await rootRedis.saddAsync('rooms', randomString)
-    return randomString
-}
-
-module.exports = {
-    createRoom
-}
 
 //TODO List
 //1. If professor logs in from a new IP, terminate the old ws
